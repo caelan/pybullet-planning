1182fa74b9723d35789055270bb5eada3608df4a
ziw <ziw@mit.edu>
2019-01-22 18:25:31 -0500

    update hide output

--- pybullet_tools/utils.py
+++ pybullet_tools/utils.py
@@ -122,22 +122,23 @@ class HideOutput(object):
     with HideOutput():
         os.system('ls -l')
     '''
-    def __init__(self, *args, **kw):
+    def __init__(self, enable=True):
+        self.enable = enable
+        if not self.enable:
+            return
         sys.stdout.flush()
-        self._origstdout = sys.stdout
-        self._oldstdout_fno = os.dup(sys.stdout.fileno())
-        self._devnull = os.open(os.devnull, os.O_WRONLY)
+        self._stdout = sys.stdout
 
     def __enter__(self):
-        self._newstdout = os.dup(1)
-        os.dup2(self._devnull, 1)
-        os.close(self._devnull)
-        sys.stdout = os.fdopen(self._newstdout, 'w')
+        if not self.enable:
+            return
+        sys.stdout = open(os.devnull, 'w')
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout = self._origstdout
-        sys.stdout.flush()
-        os.dup2(self._oldstdout_fno, 1)
+        if not self.enable:
+            return
+        sys.stdout.close()
+        sys.stdout = self._stdout
 
 #####################################
 
