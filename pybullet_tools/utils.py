22096b9d5d05555b8887b88a99ff91bd11fcc366
caelan <cgarrett@nvidia.com>
2019-07-10 16:21:05 -0700

    Min/max limits

--- pybullet_tools/utils.py
+++ pybullet_tools/utils.py
@@ -1200,8 +1200,14 @@ def get_min_limit(body, joint):
     # TODO: rename to min_position
     return get_joint_limits(body, joint)[0]
 
+def get_min_limits(body, joints):
+    return [get_min_limit(body, joint) for joint in joints]
+
 def get_max_limit(body, joint):
     return get_joint_limits(body, joint)[1]
+
+def get_max_limits(body, joints):
+    return [get_max_limit(body, joint) for joint in joints]
 
 def get_max_velocity(body, joint):
     return get_joint_info(body, joint).jointMaxVelocity
@@ -2305,6 +2311,7 @@ def get_collision_fn(body, joints, obstacles, attachments, self_collisions, disa
     # TODO: convert most of these to keyword arguments
     check_link_pairs = get_self_link_pairs(body, joints, disabled_collisions) \
         if self_collisions else []
+
     moving_links = frozenset(get_moving_links(body, joints))
     attached_bodies = [attachment.child for attachment in attachments]
     moving_bodies = [(body, moving_links)] + attached_bodies
@@ -2323,11 +2330,11 @@ def get_collision_fn(body, joints, obstacles, attachments, self_collisions, disa
         for link1, link2 in check_link_pairs:
             # Self-collisions should not have the max_distance parameter
             if pairwise_link_collision(body, link1, body, link2): #, **kwargs):
-                #print(body, link1, link2)
+                #print(get_body_name(body), get_link_name(body, link1), get_link_name(body, link2))
                 return True
         for body1, body2 in check_body_pairs:
             if pairwise_collision(body1, body2, **kwargs):
-                #print(body1, body2)
+                #print(get_body_name(body1), get_body_name(body2))
                 return True
         return False
     return collision_fn
