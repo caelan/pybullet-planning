4d60c7a2a9d2c487ded485217a3f885894f04a22
caelan <caelan.garrett@gmail.com>
2019-10-03 14:09:30 -0400

    Grippers

--- pybullet_tools/utils.py
+++ pybullet_tools/utils.py
@@ -37,12 +37,22 @@ DEFAULT_TIME_STEP = 1./240. # seconds
 
 #####################################
 
+DRAKE_PATH = 'models/drake/'
+
 # Models
 
 # Robots
+MODEL_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'models/'))
 ROOMBA_URDF = 'models/turtlebot/roomba.urdf'
 TURTLEBOT_URDF = 'models/turtlebot/turtlebot_holonomic.urdf'
-DRAKE_IIWA_URDF = "models/drake/iiwa_description/urdf/iiwa14_polytope_collision.urdf"
+DRAKE_IIWA_URDF = 'models/drake/iiwa_description/urdf/iiwa14_polytope_collision.urdf'
+WSG_50_URDF = 'models/drake/wsg_50_description/urdf/wsg_50_mesh_visual.urdf' # wsg_50 | wsg_50_mesh_visual | wsg_50_mesh_collision
+#SCHUNK_URDF = 'models/drake/wsg_50_description/sdf/schunk_wsg_50.sdf'
+PANDA_HAND_URDF = "models/franka_description/robots/hand.urdf"
+PANDA_ARM_URDF = "models/franka_description/robots/panda_arm_hand.urdf"
+
+# Pybullet Robots
+#PYBULLET_DIRECTORY = add_data_path()
 KUKA_IIWA_URDF = "kuka_iiwa/model.urdf"
 KUKA_IIWA_GRIPPER_SDF = "kuka_iiwa/kuka_with_gripper.sdf"
 R2D2_URDF = "r2d2.urdf"
@@ -50,9 +60,13 @@ MINITAUR_URDF = "quadruped/minitaur.urdf"
 HUMANOID_MJCF = "mjcf/humanoid.xml"
 HUSKY_URDF = "husky/husky.urdf"
 RACECAR_URDF = 'racecar/racecar.urdf' # racecar_differential.urdf
+PR2_GRIPPER = 'pr2_gripper.urdf'
+WSG_GRIPPER = 'gripper/wsg50_one_motor_gripper.sdf' # wsg50_one_motor_gripper | wsg50_one_motor_gripper_new
+
+# Pybullet Objects
+KIVA_SHELF_SDF = "kiva_shelf/model.sdf"
 
 # Objects
-KIVA_SHELF_SDF = "kiva_shelf/model.sdf"
 SMALL_BLOCK_URDF = "models/drake/objects/block_for_pick_and_place.urdf"
 BLOCK_URDF = "models/drake/objects/block_for_pick_and_place_mid_size.urdf"
 SINK_URDF = 'models/sink.urdf'
@@ -373,7 +387,6 @@ URDF_FLAGS = [p.URDF_USE_INERTIA_FROM_FILE,
               p.URDF_USE_SELF_COLLISION,
               p.URDF_USE_SELF_COLLISION_EXCLUDE_PARENT,
               p.URDF_USE_SELF_COLLISION_EXCLUDE_ALL_PARENTS]
-
 
 def get_model_path(rel_path): # TODO: add to search path
     directory = os.path.dirname(os.path.abspath(__file__))
