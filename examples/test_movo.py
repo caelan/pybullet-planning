3acfcd42d05c9bddbb848c0400bbf212a63b698f
caelan <caelan@mit.edu>
2019-11-06 23:43:43 -0500

    MOVO script

--- examples/test_movo.py
+++ examples/test_movo.py
@@ -3,35 +3,114 @@
 from __future__ import print_function
 
 import pybullet as p
+import numpy as np
 
 from pybullet_tools.utils import add_data_path, connect, dump_body, load_model, disconnect, wait_for_user, \
-    get_movable_joints, get_sample_fn, set_joint_positions, get_joint_name, joints_from_names, get_configuration, LockRenderer
+    get_movable_joints, get_sample_fn, set_joint_positions, get_joint_name, LockRenderer, HideOutput, \
+    joints_from_names, has_joint, set_color, get_links, BLACK, get_max_limits, get_min_limits, apply_alpha, get_extend_fn
 
-MOVO_URDF = "models/movo_description/movo.urdf"
+#MOVO_URDF = "models/movo_description/movo.urdf"
+MOVO_URDF = "models/movo_description/movo_lis.urdf"
+MOVO_URDF = "models/movo_description/movo_robotiq.urdf"
+
+# https://github.mit.edu/Learning-and-Intelligent-Systems/ltamp_pr2/blob/master/control_tools/ik/ik_tools/movo_ik/movo_robotiq.urdf
+# https://github.com/Learning-and-Intelligent-Systems/movo_ws/blob/master/src/kinova-movo-bare/movo_common/movo_description/urdf/movo.custom.urdf
+# https://github.mit.edu/Learning-and-Intelligent-Systems/ltamp_pr2/tree/master/control_tools/ik/ik_tools/movo_ik
+
+#####################################
+
+ARMS = ['right', 'left']
 
 HEAD_JOINTS = ['pan_joint', 'tilt_joint']
-LEFT_ARM_JOINTS = ['left_shoulder_pan_joint', 'left_shoulder_lift_joint', 'left_arm_half_joint', 'left_elbow_joint',
-                   'left_wrist_spherical_1_joint', 'left_wrist_spherical_2_joint', 'left_wrist_3_joint']
-LEFT_GRIPPER_JOINTS = ['left_gripper_finger1_joint', 'left_gripper_finger2_joint', 'left_gripper_finger3_joint']
-RIGHT_ARM_JOINTS = ['right_shoulder_pan_joint', 'right_shoulder_lift_joint', 'right_arm_half_joint', 'right_elbow_joint',
-                    'right_wrist_spherical_1_joint', 'right_wrist_spherical_2_joint', 'right_wrist_3_joint']
-RIGHT_GRIPPER_JOINTS = ['right_gripper_finger1_joint', 'right_gripper_finger2_joint', 'right_gripper_finger3_joint']
+
+ARM_JOINTS = ['{}_shoulder_pan_joint', '{}_shoulder_lift_joint', '{}_arm_half_joint', '{}_elbow_joint',
+              '{}_wrist_spherical_1_joint', '{}_wrist_spherical_2_joint', '{}_wrist_3_joint']
+
+KG3_GRIPPER_JOINTS = ['{}_gripper_finger1_joint', '{}_gripper_finger2_joint', '{}_gripper_finger3_joint']
+
+ROBOTIQ_GRIPPER_JOINTS = ['{}_gripper_finger1_joint', '{}_gripper_finger2_joint',
+                          '{}_gripper_finger1_inner_knuckle_joint', '{}_gripper_finger1_finger_tip_joint',
+                          '{}_gripper_finger2_inner_knuckle_joint', '{}_gripper_finger2_finger_tip_joint']
+
 TORSO_JOINTS = ['linear_joint']
 
+#PASSIVE_JOINTS = ['mid_body_joint']
 # TODO: mid_body_joint - might be passive
 # https://github.com/Kinovarobotics/kinova-movo/blob/master/movo_moveit_config/config/movo_kg2.srdf
+
+JOINTS = TORSO_JOINTS + HEAD_JOINTS
+
+#####################################
+
+def get_arm_joints(robot, arm):
+    assert arm in ARMS
+    return joints_from_names(robot, [joint_template.format(arm) for joint_template in ARM_JOINTS])
+
+def has_kg3_gripper(robot, arm):
+    assert arm in ARMS
+    kg3_joint_names = [joint_template.format(arm) for joint_template in KG3_GRIPPER_JOINTS]
+    return all(has_joint(robot, joint_name) for joint_name in kg3_joint_names)
+
+def has_robotiq_gripper(robot, arm):
+    assert arm in ARMS
+    robotiq_joint_names = [joint_template.format(arm) for joint_template in ROBOTIQ_GRIPPER_JOINTS]
+    return all(has_joint(robot, joint_name) for joint_name in robotiq_joint_names)
+
+def get_gripper_joints(robot, arm):
+    assert arm in ARMS
+    if has_kg3_gripper(robot, arm):
+        return joints_from_names(robot, [joint_template.format(arm) for joint_template in KG3_GRIPPER_JOINTS])
+    elif has_robotiq_gripper(robot, arm):
+        return joints_from_names(robot, [joint_template.format(arm) for joint_template in ROBOTIQ_GRIPPER_JOINTS])
+    raise ValueError(arm)
+
+def get_open_positions(robot, arm):
+    assert arm in ARMS
+    joints = get_gripper_joints(robot, arm)
+    if has_kg3_gripper(robot, arm):
+        return get_min_limits(robot, joints)
+    elif has_robotiq_gripper(robot, arm):
+        return 6 * [0]
+        return [0.5] + [-0.5]*5
+        return [0]*4 + get_min_limits(robot, joints[4:6]) + [0]*0
+    raise ValueError(arm)
+
+def get_closed_positions(robot, arm):
+    assert arm in ARMS
+    joints = get_gripper_joints(robot, arm)
+    if has_kg3_gripper(robot, arm):
+        return get_max_limits(robot, joints)
+    elif has_robotiq_gripper(robot, arm):
+        return [0.804]*6
+        return [0]*4 + get_max_limits(robot, joints[4:6]) + [0]*0
+    raise ValueError(arm)
+
+#####################################
+
 def main():
     # The URDF loader seems robust to package:// and slightly wrong relative paths?
     connect(use_gui=True)
     add_data_path()
 
     plane = p.loadURDF("plane.urdf")
-    #with HideOutput():
-    with LockRenderer():
-        robot = load_model(MOVO_URDF, fixed_base=True)
+    with HideOutput():
+        with LockRenderer():
+            robot = load_model(MOVO_URDF, fixed_base=True)
+            for link in get_links(robot):
+                set_color(robot, color=apply_alpha(0.2*np.ones(3), 1), link=link)
+
     dump_body(robot)
     print('Start?')
     wait_for_user()
+
+    for arm in ARMS:
+        gripper_joints = get_gripper_joints(robot, arm)
+        set_joint_positions(robot, gripper_joints, get_open_positions(robot, arm))
+    wait_for_user('Continue?')
+    for arm in ARMS:
+        gripper_joints = get_gripper_joints(robot, arm)
+        set_joint_positions(robot, gripper_joints, get_closed_positions(robot, arm))
+    wait_for_user('Continue?')
 
     #joint_names = HEAD_JOINTS
     #joints = joints_from_names(robot, joint_names)
@@ -43,7 +122,6 @@ def main():
         conf = sample_fn()
         set_joint_positions(robot, joints, conf)
         wait_for_user()
-
     disconnect()
 
 if __name__ == '__main__':
