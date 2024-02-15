8e412d40a96f085111d9473107190f3c23ea9322
Caelan Garrett <cgarrett@nvidia.com>
2024-02-15 09:25:14 -0800

    solve_multiple

--- pybullet_tools/tracik.py
+++ pybullet_tools/tracik.py
@@ -8,7 +8,8 @@ from tracikpy import TracIKSolver
 from pybullet_planning.pybullet_tools.utils import Pose, multiply, invert, tform_from_pose, get_model_info, BASE_LINK, \
     get_link_name, link_from_name, get_joint_name, joint_from_name, parent_link_from_joint, joints_from_names, \
     links_from_names, get_link_pose, draw_pose, set_joint_positions, get_joint_positions, get_joint_limits, \
-    CIRCULAR_LIMITS, get_custom_limits, irange, INF, elapsed_time
+    CIRCULAR_LIMITS, UNBOUNDED_LIMITS, get_custom_limits, irange, INF, PI, elapsed_time, is_circular, get_distance, \
+    all_between, get_difference_fn
 
 
 class IKSolver(object):
@@ -28,9 +29,10 @@ class IKSolver(object):
         # movable_joints = prune_fixed_joints(body, joints)
         # print([get_joint_name(body, joint) for joint in movable_joints])
 
+        # TODO: Distance doesn't make sense when circular limits
         self.body = body
         urdf_info = get_model_info(body)
-        self.urdf_path = os.path.abspath(urdf_info.path)
+        self.urdf_path = os.path.abspath(urdf_info.path) # self.ik_solver._urdf_string
         self.ik_solver = TracIKSolver(
             urdf_file=self.urdf_path,
             base_link=get_link_name(self.body, self.base_link),
@@ -38,8 +40,14 @@ class IKSolver(object):
             timeout=max_time, epsilon=error,
             solve_type='Speed' if speed else 'Distance', # Manipulation1 | Manipulation2
         )
-        self.joint_limits = list(get_custom_limits(
+
+        self.circular_limits = list(get_custom_limits(
             self.body, self.joints, custom_limits=custom_limits, circular_limits=CIRCULAR_LIMITS))
+        self.unbounded_limits = list(get_custom_limits(
+            self.body, self.joints, custom_limits=custom_limits, circular_limits=UNBOUNDED_LIMITS))
+        self.circular_joints = [is_circular(self.body, joint) for joint in self.joints]
+        self.default_bound = [PI if circular else INF for joint, circular in zip(self.joints, self.circular_joints)]
+        self.difference_fn = get_difference_fn(self.body, self.joints)
         self.reset_limits()
 
         self.tool_offset = tool_offset # None
@@ -67,9 +75,12 @@ class IKSolver(object):
     @property
     def joints(self):
         return joints_from_names(self.body, self.joint_names)
-    # @property
-    # def joint_limits(self):
-    #     return self.ik_solver.joint_limits
+    @property
+    def dofs(self):
+        return len(self.joints)
+    @property
+    def joint_limits(self):
+        return self.ik_solver.joint_limits
     @property
     def lower_limits(self):
         lower, _ = self.joint_limits
@@ -102,6 +113,7 @@ class IKSolver(object):
     def get_base_pose(self):
         return self.get_link_pose(self.base_link)
     def get_tool_pose(self):
+        # TODO: self.ik_solver.fk()
         return self.get_link_pose(self.tool_link)
     def world_from_base(self, pose):
         base_pose = self.get_base_pose()
@@ -123,27 +135,46 @@ class IKSolver(object):
         # TODO: truncated Gaussian
         return self.random_generator.uniform(*self.joint_limits)
     def reset_limits(self):
-        self.set_joint_limits(self.lower_limits, self.upper_limits)
+        # TODO: limits saver
+        self.set_joint_limits(*self.circular_limits)
     def set_joint_limits(self, lower, upper):
-        lower = np.maximum(lower, self.lower_limits)
-        upper = np.minimum(upper, self.upper_limits)
+        # TODO: uses -3.40282347e+38 if not set
+        lower_limits, upper_limits = self.unbounded_limits
+        lower = np.maximum(lower, lower_limits)
+        upper = np.minimum(upper, upper_limits)
         self.ik_solver.joint_limits = (lower, upper)
-    def set_nearby_limits(self, conf, bound):
-        # TODO: circular joints
+    def set_nearby_limits(self, conf, bound=None):
+        if bound is None:
+            bound = self.default_bound
         lower = np.array(conf) - bound
         upper = np.array(conf) + bound
         self.set_joint_limits(lower, upper)
         return lower, upper
+    def within_limits(self, conf):
+        return all_between(self.lower_limits, conf, self.upper_limits)
+    def adjust_conf(self, target_conf, reference_conf): # wrap_conf
+        if (target_conf is None) or (reference_conf is None):
+            return target_conf
+        # from pybullet_planning.pybullet_tools.utils import adjust_path
+        difference = self.difference_fn(target_conf, reference_conf)
+        return reference_conf + difference
 
     def solve(self, tool_pose, seed_conf=None, pos_tolerance=1e-5, ori_tolerance=math.radians(5e-2)):
         pose = self.base_from_world(tool_pose)
         tform = tform_from_pose(pose)
         #if seed_conf is None: # TODO: will use np.random.default_rng()
         #    seed_conf = self.reference_conf
+        # if seed_conf is not None:
+        #     self.set_nearby_limits(seed_conf)
         bx, by, bz = pos_tolerance * np.ones(3)
         brx, bry, brz = ori_tolerance * np.ones(3)
         conf = self.ik_solver.ik(tform, qinit=seed_conf, bx=bx, by=by, bz=bz, brx=brx, bry=bry, brz=brz)
+        # TODO: ignore limits potentially for circular joints
+        # if (seed_conf is not None) and (conf is not None):
+        #     conf = self.adjust_conf(conf, seed_conf)
+        # assert (conf is None) or self.within_limits(conf)
         self.solutions.append((pose, conf))
+        # self.reset_limits()
         return conf
     def solve_current(self, tool_pose, **kwargs): # solve_closest
         return self.solve(tool_pose, seed_conf=self.get_conf(), **kwargs)
@@ -153,24 +184,55 @@ class IKSolver(object):
         return self.solve(tool_pose, seed_conf=self.reference_conf, **kwargs)
     def solve_warm(self, tool_pose, **kwargs):
         return self.solve(tool_pose, seed_conf=self.last_solution, **kwargs)
-    def solve_multiple(self, tool_pose, seed_conf=None, max_attempts=3, max_time=INF, max_solutions=1, **kwargs):
+    def solve_multiple(self, tool_pose, seed_conf=None, max_attempts=3, max_failures=INF,
+                       max_time=INF, max_solutions=1, bound_discount=None, **kwargs):
+        # TODO: warm start from prior solution
         start_time = time.time()
+        if seed_conf is None:
+            bound_discount = None
+        nearby_conf = seed_conf
+        best_distance = INF
+        failures = 0
         solutions = []
-        for i in irange(max_attempts):
-            if (elapsed_time(start_time) >= max_time) or (len(solutions) >= max_solutions):
+        for attempt in irange(max_attempts):
+            if (elapsed_time(start_time) > max_time) or (len(solutions) > max_solutions):
                 break
-            if (i != 0) or (seed_conf is None):
+            if bound_discount is not None:
+                bound = np.minimum(bound_discount * best_distance * np.ones(self.dofs), self.default_bound) # TODO: discount parameter
+                self.set_nearby_limits(nearby_conf, bound=bound)
+            if (attempt != 0) or (seed_conf is None):
                 # TODO: modify a subset of the degrees of freedom
                 seed_conf = self.sample_conf()
             conf = self.solve(tool_pose, seed_conf=seed_conf, **kwargs)
-            if conf is not None:
-                solutions.append(conf)
-        return solutions
+
+            if conf is None:
+                failures += 1
+                if failures > max_failures:
+                    break
+                continue
+            failures = 0
+            distance = INF
+            if bound_discount is not None:
+                difference = self.difference_fn(conf, nearby_conf)
+                distance = np.linalg.norm(difference, ord=INF)
+                best_distance = min(distance, best_distance)
+                print(f'Iteration: {attempt}/{max_attempts} | Current: {distance:.3f} | Best: {best_distance:.3f} | '
+                      f'Elapsed: {elapsed_time(start_time):.3f}')
+            solutions.append((conf, distance))
+        self.reset_limits()
+        solutions.sort(key=lambda p: p[1], reverse=False)
+        return [conf for conf, _ in solutions]
     def solve_restart(self, tool_pose, **kwargs):
         solutions = self.solve_multiple(tool_pose, max_solutions=1, **kwargs)
         if not solutions:
             return None
         return solutions[0]
+    def solve_distance(self, tool_pose, seed_conf, max_attempts=INF, max_time=0.1,
+                       max_failures=0, bound_discount=0.95, **kwargs):
+        # TODO: use the ratio of weights to adjust the distance
+        # TODO: shrink all (L-inf) vs one coordinate
+        return self.solve_restart(tool_pose, seed_conf=seed_conf, max_attempts=max_attempts, max_time=max_time,
+                                  max_solutions=INF, max_failures=max_failures, bound_discount=bound_discount, **kwargs)
     def generate(self, tool_pose, include_failures=False, **kwargs):
         #start_time = time.time()
         while True:
